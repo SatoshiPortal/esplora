460a91cb4e8d604d9cf88394936c8a5a634ff2da
kexkey <github@kexkey.com>
2019-03-09 02:45:48 -0500

    Added TOR support

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ cd esplora
 sed -e 's/electrs --timestamp/electrs -vvvv --timestamp/' contrib/supervisord.conf.in -i
 docker build -t esplora .
 cd ..
+mkdir -p tor/hidden_service
+chmod 700 tor/hidden_service
+sudo chown 2000:2000 tor/hidden_service
