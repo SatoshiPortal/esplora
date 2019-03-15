03e7e7514b839511bc864e62c6e292ff0b073638
kexkey <github@kexkey.com>
2019-03-14 21:45:45 -0400

    Removed comment

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 git clone https://github.com/blockstream/esplora
 cp -r esplora-add/* esplora
 cd esplora
-#sed -e 's/electrs --timestamp/electrs -vvvv --timestamp/' contrib/supervisord.conf.in -i
 docker build -t esplora .
 cd ..
 mkdir -p tor/hidden_service
